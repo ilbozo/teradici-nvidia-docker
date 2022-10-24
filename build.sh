42ac689ee7d1dfebeda9b03f32ce3e2f0097abdf
Alessandro Boschian Pest <ilbozo@gmail.com>
2022-10-24 18:04:01 +0200

    Update build.sh
    fixed wrong if/else statement

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ex
 if command -v nvidia-smi; then
     echo "NVidia driver detected, using nvidia/cudagl image"
     UPSTREAM_IMAGE=nvidia/cudagl:11.4.2-runtime-ubuntu20.04
+else
     echo "NVidia driver not found, using regular ubuntu image"
     UPSTREAM_IMAGE=ubuntu:20.04
 fi
